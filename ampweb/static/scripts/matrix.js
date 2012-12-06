51f15990487207059c0be710ac392f9052cc63b1
Jeremy Utting <crunchybar@gmail.com>
2012-12-07 10:53:04 +1300

    added some classes and styling to the matrix grid, and some basic DataTables configuration

--- ampweb/static/scripts/matrix.js
+++ ampweb/static/scripts/matrix.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-    $('#AMP_matrix').dataTable();
+    $('#AMP_matrix').dataTable({
+        "bInfo": false, //disable table information
+        "bProcessing": true, //enabling processing indicator
+        "bSortClasses": false, //disable the addition of sorting classes
+        "bStateSave": true, //saves user table state in a cookie
+        "bPaginate": false //disable pagination
+    });
 });
