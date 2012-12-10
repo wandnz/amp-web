d536b0f878dfc4213fa3ed94bd8a699cd1a5a11e
Chris <chris.lloyd.hi@gmail.com>
2012-12-10 15:59:46 +1300

    Deals with errors thrown by ampy. It also puts the data in a nice format with "response" or "error" keys and descriptions for the lists of data you're getting back. While it is not an exact copy of the current REST API, it's pretty close. The data isn't in the same format though, so it won't be exactly the same.

--- ampweb/views/data.py
+++ ampweb/views/data.py
@@ -1,9 +1,3 @@
-#
-#
-############ THIS IS A TEMPORARY FILE AND WILL BE REPLACED WHEN
-############ A PROPER REST API IS IMPELEMTED (GRAPH PAGE USES THIS)
-#
-#
 from pyramid.response import Response
 from pyramid.view import view_config
 from ampy import ampdb
@@ -22,24 +16,45 @@ def graph(request):
     
     db = ampdb.create()
 
+    #Used to determine what type of response is to be returned
+    successnumber = 0
+
     try:
         source = urlparts[0]
+        successnumber += 1
         dest = urlparts[1]
+        successnumber += 1
         test = urlparts[2]
+        successnumber += 1
         options = urlparts[3]
+        successnumber += 1
         start = int(urlparts[4])
+        successnumber += 1
         end = int(urlparts[5])
+        successnumber += 1
     except:
         pass
 
-    data = db.get(source, dest, test, options, start, end)
-    
-    response = []    
+    #What type of response is it
+    type = {0 : "Error",
+            1 : "sites",
+            2 : "sites",
+            3 : "tests",
+            4 : "subtypes",
+            5 : "Should not exist",
+            6 : "Should not exist",
+           }
 
-    for daata in data:
-        response.append(daata)
-    
-    #response = json.dumps(response)
+    response = {}
+    try:
+        data = db.get(source, dest, test, options, start, end)
+    except:
+        response["error"] = "There was an error"
+    else:
+        response["response"] = {}
+        response["response"][type[successnumber]] = []
+        for daata in data:
+            response["response"][type[successnumber]].append(daata)
     
     #RETURN
     return response
