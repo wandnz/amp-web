1a2bd195617a51b1463ed8d6d2b5e7e1dd89137e
Jeremy Utting <crunchybar@gmail.com>
2012-12-17 16:05:04 +1300

    Implemented tabs to switch between different data types, and improved the mapping between the URL and the displayed resource

--- ampweb/static/scripts/matrix.js
+++ ampweb/static/scripts/matrix.js
@@ -1,43 +1,107 @@
 var matrix;
 $(document).ready(function(){
     /*
-        This is the initialization code for the dataTable
-    */
+     * This function intializes the jqueryui tabs
+     */
+    $(function() {
+        $("#topTabs").tabs();
+    });
+    
+    /* pull the current URI and split into segments */
+    var URI_init = new URI(window.location);
+    var segments = URI_init.segment();
+
+    /* FIXME: Works for now, but this code is horrible and repetitive */
+    if (segments.length == 1) {
+        URI_init.segment(0, "matrix");
+        URI_init.segment(1, "latency");
+        URI_init.segment(2, "nz");
+        URI_init.segment(3, "nz");
+    }
+    else if (segments.length == 2) {
+        if (validTestType(segments[1])) {
+            /* TODO: find a way to select the appropriate tab */
+        }
+        else {
+            URI_init.segment(1, "latency");
+        }
+        URI_init.segment(2, "nz");
+        URI_init.segment(3, "nz");
+    }
+    else if (segments.length == 3) {
+        if (validTestType(segments[1])) {
+            /* select tab */
+        }
+        else {
+            URI_init.segment(1, "latency");
+        }
+        URI_init.segment(3, "nz");
+    }
+    else if (segments.length >= 4) {
+        if (validTestType(segments[1])) {
+            /* select tab */
+        }
+        else {
+            URI_init.segment(1, "latency");
+        }
+    }
+    window.history.pushState("", "", URI_init.toString());
+
+    /*
+     * These funtions add onclick handlers for each jqueryui tab
+     */
+    $("#latencyTab").click(function() {
+        updateURI(1, "latency");
+    });
+
+    $("#lossTab").click(function() {
+        updateURI(1, "loss");
+    });
+
+    $("#hopsTab").click(function() {
+        updateURI(1, "hops");
+    });
+
+    $("#mtuTab").click(function() {
+        updateURI(1, "mtu");
+    });
+
+    /*
+     * This is the initialization code for the dataTable
+     */
     matrix = $('#AMP_matrix').dataTable({
-        "bInfo": false, //disable table information
-        "bProcessing": true, //enabling processing indicator
-        "bStateSave": true, //saves user table state in a cookie
-        "bPaginate": false, //disable pagination
-        "bJQueryUI": true, //enable JQuery UI for ThemeRoller support
-        "sAjaxSource": "/update_matrix", //get ajax data from this source
+        "bInfo": false, /* disable table information */
+        "bProcessing": true, /* enabling processing indicator */
+        "bStateSave": true, /* saves user table state in a cookie */
+        "bPaginate": false, /* disable pagination */
+        "bJQueryUI": true, /* enable JQuery UI for ThemeRoller support */
+        "sAjaxSource": "/update_matrix", /* get ajax data from this source */
         /*
-            overrides the default function for getting the data from the server,
-            so that we can pass data in the ajax request 
-        */
+         * overrides the default function for getting the data from the server,
+         * so that we can pass data in the ajax request 
+         */
         "fnServerData": function(sSource, aoData, fnCallback) {
-            //load up some default values, incase none are specified
+            /* load up some default values, in-case none are specified */
             var src = "nz";
             var dst = "nz";
-            var ipVersion = "ipv4";
-            var test = "icmp";
-            var uri = new URI(window.location); //pull the current url
-            var segments = uri.segment(); //split the url path into segments
+            var test = "latency";
+            /* pull the current url */
+            var uri = new URI(window.location);
+            /* split the url path into segments */
+            var segments = uri.segment();
 
+            /* FIXME: works, but not happy with how this is done */
             if((segments[1] != undefined) && (segments[1] != "")) {
-                ipVersion = segments[1];
+                test = segments[1];
                 if((segments[2] != undefined) && (segments[2] != "")) {
-                    test = segments[2];
+                    src = segments[2];
                     if((segments[3] != undefined) && (segments[3] !=  "")) {
-                        src = segments[3];
-                        if((segments[4] != undefined) && (segments[4] != "")) {
-                            dst = segments[4];
-                        }
+                        dst = segments[3];
                     }
                 }
             }
             
-            //push the values into the GET data
-            aoData.push({"name": "ipVersion", "value": ipVersion});
+            /* push the values into the GET data */
             aoData.push({"name": "testType", "value": test});
             aoData.push({"name": "source", "value": src});
             aoData.push({"name": "destination", "value": dst});
@@ -50,19 +114,39 @@ $(document).ready(function(){
             });
         }
     });
-    setInterval("reDraw()", 30000); //tells the table how often to refresh
+    /* tells the table hwo often to refresh */
+    setInterval("reDraw()", 1000);
 });
 
 /*
-    This function is periodically called to redraw the table
-    with new data fetched via ajax
-*/
+ * This function is periodically called to redraw the table
+ * with new data fetched via ajax
+ */
 function reDraw() {
     matrix.fnReloadAjax();
 }
 
-$(function() {
-    $("#topTabs").tabs();
-    $("#bottomTabs").tabs();
-});
+/*
+ * This function takes a position and a value, and updates the 
+ * given position within the URL's path, with the given value
+ */
+function updateURI(position, value) {
+    var currentURI = new URI(window.location);
+    currentURI.segment(position, value);
+    window.history.pushState("", "", currentURI.toString());
 
+}
+
+/*
+ * This function takes a value, and checks it against a list 
+ * of valid test types, and returns true or false
+ * FIXME: works, but I don't like how static it is
+ */
+function validTestType(value) {
+    if (value == "latency" || value == "loss" || value == "hops" || value == "mtu") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
--- ampweb/views/matrix_update.py
+++ ampweb/views/matrix_update.py
@@ -5,29 +5,50 @@ from time import time
 
 @view_config(renderer='json', route_name='matrix_update')
 def home(request):
+    binSize = 3600
+    global ampyTest
+    global subtest
+    global index
+    global sub_index
     src = request.params['source']
     dst = request.params['destination']
-    ipVersion = request.params['ipVersion'] #not used yet
     test = request.params['testType']
-    
+    if test == "latency":
+        ampyTest = "icmp"
+        subtest = "0084"
+        index = "rtt_ms"
+        sub_index = "mean"
+    elif test == "loss":
+        ampyTest = "icmp"
+        subtest = "0084"
+        index = "rtt_ms"
+        sub_index = "missing"
+    elif test == "hops": # The data is not available for this yet
+        pass 
+    elif test == "mtu": # The data is not available for this yet
+        pass
+
     conn = ampdb.create()
     srcData = conn.get()
     dstData = conn.get(srcData)
     srcList = srcData.fetchall()
     dstList = dstData.fetchall()
     currentTime = int(time())
-   
+
     tableData = []
+    # Query for data between every source and destination
     for src in srcList:
         rowData = [src]
         for dst in dstList:
-            #static(ish) query, for now
-            result = conn.get(src, dst, test, "0084", currentTime-30, currentTime, 1000)
-            queryDataMean = result.fetchone()['rtt_ms']['mean']
-            rowData.append(queryDataMean)
+            result = conn.get(src, dst, ampyTest, subtest, currentTime-60, currentTime, binSize)   
+            queryData = result.fetchone()[index][sub_index]
+            if test == "loss":
+                loss = (100.0 * queryData) / (binSize / 60)
+                queryData = str(round(loss, 1)) + "%"
+            rowData.append(queryData)
         tableData.append(rowData)
 
-    #create a dictionary so that the data is stored in a way that DataTables expects
+    # Create a dictionary so that the data is stored in a way that DataTables expects
     data_list_dict = {}
     data_list_dict.update({'aaData': tableData})
     return data_list_dict
