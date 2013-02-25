3de4c2f1392e08d1e2827ff1a165fee182497d6c
Brendon Jones <brendonj@waikato.ac.nz>
2013-02-25 17:16:05 +1300

    Remove unnecessary code from the matrix view.

--- ampweb/views/matrix.py
+++ ampweb/views/matrix.py
@@ -1,4 +1,3 @@
-from pyramid.response import Response
 from pyramid.renderers import get_renderer
 from pyramid.view import view_config
 from ampy import ampdb
@@ -6,50 +5,20 @@ from ampy import ampdb
 
 @view_config(route_name='home', renderer='../templates/skeleton.pt')
 @view_config(route_name='matrix', renderer='../templates/skeleton.pt')
+
 def home(request):
     page_renderer = get_renderer("../templates/matrix.pt")
     body = page_renderer.implementation().macros['body']
 
-    url = None
-
-    if 'params' in request.matchdict:
-        url = request.matchdict['params']
-
-    # Connect to the ampdb
     db = ampdb.create()
 
-    # Default values
-    dataType = "latency"
-    src = "nz"
-    dst = "nz"
-
-    # TODO: pull the src/dst from the URL? 
-
-    # Get the lists of src & dst meshes
-    srcMeshes = db.get_source_meshes()
-    dstMeshes = db.get_destination_meshes()
-
-    # Fetch all available sources and destinations in the desired mesh.
-    srcList = db.get_sources(mesh=src)
-    dstListUntrimmed = db.get_destinations(mesh=dst)
-    dstList = []
-
-    for destination in dstListUntrimmed:
-        if destination.startswith("ampz-"):
-            dstList.append(destination[5:])
-        else:
-            dstList.append(destination)
-            
-            
     return {
         "title": "AMP Measurements",
-        "body": body, 
-        "scripts": SCRIPTS, 
-        "styles": STYLES, 
-        "srcList": srcList, 
-        "dstList": dstList,
-        "srcMeshes": srcMeshes,
-        "dstMeshes": dstMeshes
+        "body": body,
+        "scripts": SCRIPTS,
+        "styles": STYLES,
+        "srcMeshes": db.get_source_meshes(),
+        "dstMeshes": db.get_destination_meshes()
     }
 
 SCRIPTS = [
