9adaa79762899f2209391aeac799cc0de5cb742d
Brendon Jones <brendonj@waikato.ac.nz>
2013-06-18 11:57:34 +1200

    Extract the event labelling/display code into a separate file.

--- ampweb/views/dashboard.py
+++ ampweb/views/dashboard.py
@@ -2,64 +2,10 @@ from pyramid.view import view_config
 from pyramid.renderers import get_renderer
 from ampy import ampdb
 import time
+import eventlabels
 
 STYLES = []
 
-def get_site_count_label(site_count):
-    """ Properly format the number of sites involved in events for a label """
-    if site_count == 1:
-        return "1 site"
-    return "%d sites" % site_count
-
-def get_event_count_label(event_count):
-    """ Properly format the number of events for a label """
-    if event_count == 1:
-        return "1 event"
-    return "%d events" % event_count
-
-def get_event_label(event):
-    """ Properly format the time and description of an event for a label """
-    if event["collection_style"] == "smokeping":
-        return get_smokeping_event_label(event)
-    if event["collection_style"] == "muninbytes":
-        return get_muninbytes_event_label(event)
-
-def get_smokeping_event_label(event):
-    """ Properly format the time and description of a smokeping event """
-    label = "Smokeping: " + event["event_time"].strftime("%H:%M:%S")
-    label += " %s " % event["type_name"]
-    label += "from %s to %s" % (event["source_name"], event["target_name"])
-    label += ", severity level = %s/100" % event["severity"]
-    return label
-
-def get_muninbytes_event_label(event):
-    """ Properly format the time and description of a muninbytes event """
-    label = "munin: " + event["event_time"].strftime("%H:%M:%S")
-    label += " %s " % event["type_name"]
-    label += "from %s to %s" % (event["source_name"], event["target_name"])
-    label += ", severity level = %s/100" % event["severity"]
-    return label
-
-def get_event_href(event):
-    """ Build the link to the graph showing an event """
-    if event["collection_style"] == "smokeping":
-        return get_smokeping_event_href(event)
-    if event["collection_style"] == "muninbytes":
-        return get_muninbytes_event_href(event)
-
-def get_smokeping_event_href(event):
-    """ Build the link to the graph showing a smokeping event """
-    start = event["timestamp"] - (3 * 60 * 60)
-    end = event["timestamp"] + (1 * 60 * 60)
-    href = "/graph/smokeping/%s/30/%d/%d" % (event["stream_id"], start, end)
-    return href
-
-def get_muninbytes_event_href(event):
-    """ Build the link to the graph showing a muninbytes event """
-    start = event["timestamp"] - (3 * 60 * 60)
-    end = event["timestamp"] + (1 * 60 * 60)
-    href = "/graph/muninbytes/%s/30/%d/%d" % (event["stream_id"], start, end)
-    return href
 
 @view_config(route_name="dashboard", renderer="../templates/skeleton.pt")
 def dashboard(request):
@@ -89,8 +35,8 @@ def dashboard(request):
         # build the label describing roughly what the event group contains
         label = group["group_start_time"].strftime("%H:%M:%S %A %B %d %Y")
         label += " (%s, %s)" % (
-                get_site_count_label(group["group_site_count"]),
-                get_event_count_label(group["group_event_count"])
+                eventlabels.get_site_count_label(group["group_site_count"]),
+                eventlabels.get_event_count_label(group["group_event_count"])
                 )
 
         # get all the events in the event group ready for display
@@ -99,9 +45,9 @@ def dashboard(request):
         for event in group_events:
             # insert most recent events at the front of the list
             events.insert(0, {
-                "label": get_event_label(event),
+                "label": eventlabels.get_event_label(event),
                 "description": event["event_description"],
-                "href": get_event_href(event),
+                "href": eventlabels.get_event_href(event),
             })
 
         # add the most recent event groups at the front of the list
