851f5c94c26e5416e5882ff8706df45def4fd362
Brendon Jones <brendonj@waikato.ac.nz>
2013-06-18 15:12:05 +1200

    Try to fix some of the bizarre indentation that happened somehow.

--- ampweb/static/scripts/smokeping.js
+++ ampweb/static/scripts/smokeping.js
@@ -21,9 +21,9 @@
  */
 Flotr.addType('smoke', {
     options: {
-	show: false,     // => setting to true will show lines
-	medianLineWidth: 3,
-	verticalLineWidth: 1,
+        show: false,     // => setting to true will show lines
+        medianLineWidth: 3,
+        verticalLineWidth: 1,
     },
 
     /**
@@ -32,14 +32,14 @@ Flotr.addType('smoke', {
      */
     draw : function (options) {
 
-	var context = options.context;
+        var context = options.context;
 
-	context.save();
-	context.lineJoin = 'round';
+        context.save();
+        context.lineJoin = 'round';
 
-	this.plot(options, 0, true);
+        this.plot(options, 0, true);
 
-	context.restore();
+        context.restore();
     },
 
     /**
@@ -47,117 +47,120 @@ Flotr.addType('smoke', {
      */
     plot : function (options, shadowOffset, incStack) {
 
-	var
-	context   = options.context,
-	width     = options.width,
-	height    = options.height,
-	xScale    = options.xScale,
-	yScale    = options.yScale,
-	data      = options.data,
-	medianLineWidth = options.medianLineWidth,
-	verticalLineWidth = options.verticalLineWidth,
-	length    = data.length - 1,
-	prevx     = null,
-	prevy     = null,
-	x1, x2, y1, y2, i, median, ping, measurements, loss;
+        var
+            context   = options.context,
+            width     = options.width,
+            height    = options.height,
+            xScale    = options.xScale,
+            yScale    = options.yScale,
+            data      = options.data,
+            medianLineWidth = options.medianLineWidth,
+            verticalLineWidth = options.verticalLineWidth,
+            length    = data.length - 1,
+            prevx     = null,
+            prevy     = null,
+            x1, x2, y1, y2, i, median, ping, measurements, loss;
 
-	if (length < 1) return;
+        if ( length < 1 ) {
+            return;
+        }
 
-	context.beginPath();
+        context.beginPath();
 
-	for (i = 0; i < length; ++i) {
-	    /* To allow empty values */
-	    if (data[i][1] === null || data[i+1][1] === null) {
-		continue;
-	    }
+        for ( i = 0; i < length; ++i ) {
+            /* To allow empty values */
+            if ( data[i][1] === null || data[i+1][1] === null ) {
+                continue;
+            }
 
-	    /* data should have at least [timestamp,median,loss] */
-	    if ( data[i].length < 3 ) {
-		continue;
-	    }
+            /* data should have at least [timestamp,median,loss] */
+            if ( data[i].length < 3 ) {
+                continue;
+            }
 
-	    x1 = xScale(data[i][0]);
-	    x2 = xScale(data[i+1][0]);
+            x1 = xScale(data[i][0]);
+            x2 = xScale(data[i+1][0]);
 
-	    measurements = data[i].length,
-	    loss = data[i][2];
-	    median = data[i][1];
-	    y1 = yScale(median);
-	    y2 = yScale(data[i+1][1]);
+            measurements = data[i].length,
+            loss = data[i][2];
+            median = data[i][1];
+            y1 = yScale(median);
+            y2 = yScale(data[i+1][1]);
 
-	    if (
-		    (y1 > height && y2 > height) ||
-		    (y1 < 0 && y2 < 0) ||
-		    (x1 < 0 && x2 < 0) ||
-		    (x1 > width && x2 > width)
-	       ) continue;
+            if (
+                (y1 > height && y2 > height) ||
+                (y1 < 0 && y2 < 0) ||
+                (x1 < 0 && x2 < 0) ||
+                (x1 > width && x2 > width)
+               ) continue;
 
-	    if((prevx != x1) || (prevy != y1 + shadowOffset))
-		context.moveTo(x1, y1 + shadowOffset);
+            if ( (prevx != x1) || (prevy != y1 + shadowOffset) ) {
+                context.moveTo(x1, y1 + shadowOffset);
+            }
 
-	    prevx = x2;
-	    prevy = y2 + shadowOffset;
-
-
-	    /*
-	     * Draw smoke around the median if the data is available. If we
-	     * draw this first then all the coloured lines get drawn on top,
-	     * without being obscured.
-	     */
-	    context.fillStyle = 'rgba(0,0,0,0.2)';
-	    /* TODO is this going to be really slow? */
-	    for (j = 3; j < measurements; j++) {
-		ping = data[i][j];
-		if ( ping == null ) {
-		    continue;
-		}
-		/* draw a rectangle for every non-median measurement */
-		if ( ping != median ) {
-		    context.fillRect(x1, y1, x2-x1,
-			    yScale(ping) - yScale(median));
-		}
-	    }
+            prevx = x2;
+            prevy = y2 + shadowOffset;
 
 
-	    /* use the loss data to colour the line */
-	    context.beginPath();
-	    context.lineWidth = medianLineWidth;
-
-	    /*
-	     * Colours are based on the smokeping loss colours, though the
-	     * ranges that they cover are slightly different. Any loss above
-	     * 10 of the 20 packets gets red.
-	     */
-	    if ( loss < 1 ) {
-		context.strokeStyle = 'rgba(0, 255, 0, 1.0)';
-	    } else if ( loss < 2 ) {
-		context.strokeStyle = 'rgba(0, 184, 255, 1.0)';
-	    } else if ( loss < 3 ) {
-		context.strokeStyle = 'rgba(0, 89, 255, 1.0)';
-	    } else if ( loss < 4 ) {
-		context.strokeStyle = 'rgba(94, 0, 255, 1.0)';
-	    } else if ( loss < 5 ) {
-		context.strokeStyle = 'rgba(126, 0, 255, 1.0)';
-	    } else if ( loss < 10 ) {
-		context.strokeStyle = 'rgba(221, 0, 255, 1.0)';
-	    } else {
-		context.strokeStyle = 'rgba(255, 0, 0, 1.0)';
-	    }
-
-	    /* draw horizontal line for the median measurement */
-	    context.moveTo(x1, y1 + shadowOffset);
-	    context.lineTo(prevx + shadowOffset / 2, y1+shadowOffset);
-	    context.stroke();
+            /*
+             * Draw smoke around the median if the data is available. If we
+             * draw this first then all the coloured lines get drawn on top,
+             * without being obscured.
+             */
+            context.fillStyle = 'rgba(0,0,0,0.2)';
+            /* TODO is this going to be really slow? */
+            for ( j = 3; j < measurements; j++ ) {
+                ping = data[i][j];
+                if ( ping == null ) {
+                    continue;
+                }
+                /* draw a rectangle for every non-median measurement */
+                if ( ping != median ) {
+                    context.fillRect(x1, y1, x2-x1,
+                        yScale(ping) - yScale(median));
+                }
+            }
 
 
-	    /* draw thin black vertical line between measurements */
-	    context.beginPath();
-	    context.strokeStyle = 'rgba(0, 0, 0, 1.0)';
-	    context.lineWidth = verticalLineWidth;
-	    context.moveTo(prevx + shadowOffset / 2, y1+shadowOffset);
-	    context.lineTo(prevx + shadowOffset / 2, prevy);
-	    context.stroke();
-	}
+            /* use the loss data to colour the line */
+            context.beginPath();
+            context.lineWidth = medianLineWidth;
+
+            /*
+             * Colours are based on the smokeping loss colours, though the
+             * ranges that they cover are slightly different. Any loss above
+             * 10 of the 20 packets gets red.
+             */
+            if ( loss < 1 ) {
+                context.strokeStyle = 'rgba(0, 255, 0, 1.0)';
+            } else if ( loss < 2 ) {
+                context.strokeStyle = 'rgba(0, 184, 255, 1.0)';
+            } else if ( loss < 3 ) {
+                context.strokeStyle = 'rgba(0, 89, 255, 1.0)';
+            } else if ( loss < 4 ) {
+                context.strokeStyle = 'rgba(94, 0, 255, 1.0)';
+            } else if ( loss < 5 ) {
+                context.strokeStyle = 'rgba(126, 0, 255, 1.0)';
+            } else if ( loss < 10 ) {
+                context.strokeStyle = 'rgba(221, 0, 255, 1.0)';
+            } else {
+                context.strokeStyle = 'rgba(255, 0, 0, 1.0)';
+            }
+
+            /* draw horizontal line for the median measurement */
+            context.moveTo(x1, y1 + shadowOffset);
+            context.lineTo(prevx + shadowOffset / 2, y1+shadowOffset);
+            context.stroke();
+
+
+            /* draw thin black vertical line between measurements */
+            context.beginPath();
+            context.strokeStyle = 'rgba(0, 0, 0, 1.0)';
+            context.lineWidth = verticalLineWidth;
+            context.moveTo(prevx + shadowOffset / 2, y1+shadowOffset);
+            context.lineTo(prevx + shadowOffset / 2, prevy);
+            context.stroke();
+        }
     },
 
     /**
@@ -179,9 +182,9 @@ Flotr.addType('smoke', {
 	    var j;
 	    /* measurements may be [timestamp,median,loss,ping1, ping2 ...] */
 	    for ( j = 3; j < data[i].length; j++ ) {
-		if ( data[i][j] > axis.max ) {
-		    axis.max = data[i][j];
-		}
+            if ( data[i][j] > axis.max ) {
+                axis.max = data[i][j];
+            }
 	    }
 	}
     /*
