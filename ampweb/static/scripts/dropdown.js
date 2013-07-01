f4f0b3ddb8d7afa713eb8314cb51d07aa6e97d5b
Shane Alcock <salcock@waikato.ac.nz>
2013-07-01 16:56:09 +1200

    Replaced dropdown code with OO implementation
    The dropdown list code was getting unmanagable so I've replaced it with a
    much nicer object-oriented implementation. Now it should be easier to add
    dropdown lists for new collections -- just implement a new Dropdown subclass
    with all the required methods and make sure the appropriate .js file is
    included by the graph view.
    
    Much better than having to edit a crapload of switch statements all over the
    place to make sure the right dropdown function is called for the collection
    you are trying to add.

--- ampweb/static/scripts/dropdown.js
+++ ampweb/static/scripts/dropdown.js
@@ -1,251 +1,59 @@
-var smokepingSource = "";
-var smokepingDest = "";
+function Dropdown() {
+    this.sortDropdown = function(ddName, selected) {
+        var r1 = $(ddName + " option");
+        r1.sort( function(a, b) {
+            if (a.text < b.text) return -1;
+            if (a.text == b.text) return 0;
+            return 1;
+        });
+        $(r1).remove();
+        $(ddName).append($(r1));
 
-var munin = {"switch":"", "interface":"", "direction":""};
-
-function sortDropdown(ddName, selected) {
-    var r1 = $(ddName + " option");
-    r1.sort( function(a, b) {
-        if (a.text < b.text) return -1;
-        if (a.text == b.text) return 0;
-        return 1;
-    });
-    $(r1).remove();
-    $(ddName).append($(r1));
-   
-    /* Set the selected value to our "please select" option */ 
-    if (selected == "") {
-        selected = "--SELECT--";
-    }
-    
-    /* VERY IMPORTANT: don't use object:contains() here! It doesn't do an
-     * exact match, so if you have two text options with similar names you can
-     * often end up with the wrong option being selected.
-     *
-     * The below approach is much more explicit and will perform an exact
-     * match only.
-     */
-    $(ddName + " option").each(function() {
-        if ($(this).text() == selected) {
-            $(this).attr('selected', 'selected');
-            return;
-        } else {
-            $(this).attr('selected', false);
+        /* Set the selected value to our "please select" option */
+        if (selected == "") {
+            selected = "--SELECT--";
         }
-    });
-}
 
-function populateDropdown(name, data, selected) {
-    /* Clear the current population */
-    $(name).empty();
-    $(name).append(
-            "<option value=\"--SELECT--\">--SELECT--</option>");
-    $.each(data, function(index, dst){
-            $("<option value=\"" + dst + "\">" + dst +
-                "</option>").appendTo(name);
-            });
-
-    /* Enable the dropdown */
-    $(name).removeAttr('disabled');
-    sortDropdown(name, selected);
-
-}
-
-function muninGetSelected() {
-    if ($("#drpSwitch option:selected").text() != "--SELECT--") {
-        munin["switch"] = $("#drpSwitch option:selected").text();
-    } else {
-        munin["switch"] = "";
-    }
-    if ($("#drpInterface option:selected").text() != "--SELECT--") {
-        munin["interface"] = $("#drpInterface option:selected").text();
-    } else {
-        munin["interface"] = "";
-    }
-
-    if ($("#drpDirection option:selected").text() != "--SELECT--") {
-        munin["direction"] = $("#drpDirection option:selected").text();
-    } else {
-        munin["direction"] = "";
-    }
-}
-
-function smokepingGetSelected() {
-
-    if ($("#drpSource option:selected").text() != "--SELECT--") {
-        smokepingSource = $("#drpSource option:selected").text();
-    } else {
-        smokepingSource = "";
-    }
-
-    if ($("#drpDest option:selected").text() != "--SELECT--") {
-        smokepingDest = $("#drpDest option:selected").text();
-    } else {
-        smokepingDest = "";
-    }
-
-
-}
-
-function getSmokepingDropdownState() {
-    var obj = {
-        type: "smokeping",
-        source:  smokepingSource,
-        dest: smokepingDest,
-    };
-
-    return obj;
-
-}
-
-function getMuninDropdownState() {
-    var obj = {
-        "type" : "munin",
-        "switch" : munin["switch"],
-        "interface":  munin["interface"],
-        "direction" : munin["direction"]
-    };
-
-    return obj;
-
-}
-
-function setSmokepingDropdownState(state) {
-
-    smokepingSource = state["source"];
-    smokepingDest = state["dest"];
-    sortDropdown("#drpSource", smokepingSource);
-    sortDropdown("#drpDest", smokepingDest);
-
-}
-
-function setMuninDropdownState(state) {
-
-    munin["switch"] = state["switch"];
-    munin["interface"] = state["interface"];
-    munin["direction"] = state["direction"];
-    sortDropdown("#drpSwitch", munin["switch"]);
-    sortDropdown("#drpInterface", munin["interface"]);
-    sortDropdown("#drpDirection", munin["direction"]);
-}
-
-
-function initMuninDropdown() {
-
-    muninGetSelected();
-    sortDropdown("#drpSwitch", munin["switch"]);
-    sortDropdown("#drpInterface", munin["interface"]);
-    sortDropdown("#drpDirection", munin["direction"]);
-}
-
-function initSmokepingDropdown(currentstream) {
-
-    smokepingGetSelected();
-    sortDropdown("#drpSource", smokepingSource);
-    sortDropdown("#drpDest", smokepingDest);
-
-}
-
-function muninDropdownCB(object) {
-
-    muninGetSelected();
-
-    if (object.name == "switch") {
-        $("#drpInterface").empty();
-        $('<option value="--SELECT--">--SELECT--</option>').appendTo("#drpInterface");
-        $("#drpInterface").attr('disabled', '');
-        $("#drpDirection").empty();
-        $('<option value="--SELECT--">--SELECT--</option>').appendTo("#drpDirection");
-        $("#drpDirection").attr('disabled', '');
-    
-        munin["interface"] = "";
-        munin["direction"] = "";
-            
-        if (munin["switch"] != "") {
-            /* Populate the interfaces dropdown */
-
-            $.ajax({
-                url: "/api/_destinations/rrd-muninbytes/" + munin["switch"] + "/",
-                success: function(data) {
-                    populateDropdown("#drpInterface", data, munin["interface"]);
-                }
-            });
-        } 
-    }
-
-    if (object.name == "interface") {
-        $("#drpDirection").empty();
-        $('<option value="--SELECT--">--SELECT--</option>').appendTo("#drpDirection");
-        $("#drpDirection").attr('disabled', '');
-        
-        munin["direction"] = "";
-        if (munin["interface"] != "") {
-            /* Populate the directions dropdown */
-
-            $.ajax({
-                url: "/api/_destinations/rrd-muninbytes/" + munin["switch"] + "/" + munin["interface"] + "/",
-                success: function(data) {
-                    populateDropdown("#drpDirection", data, munin["direction"]);
-                }
-            });
-        } 
-    }
-
-    if (munin["direction"] != "") {
-        $.ajax({
-            url: "/api/_streams/rrd-muninbytes/" + munin["switch"] + "/" + munin["interface"] + "/" + munin["direction"] + "/",
-            success: function(data) {
-                changeGraph({graph:"rrd-muninbytes", stream:data});
-                updatePageURL(true);
-
-            }
-       });
-
-    }
-
-}
-
-function smokepingDropdownCB(object) {
-
-    smokepingGetSelected();
-
-    /* Second Dropdown */
-    if (object.name == "source" && object.value != "--SELECT--") {
-        $("#drpDest").empty();
-        $("#drpDest").append("<option value=\"loading...\">Loading...</option>");
-        $("#drpDest").attr('disabled', '');
-        
-        smokepingDest = "";
-
-        /* Get data, update box */
-        $.ajax({
-            url: "/api/_destinations/rrd-smokeping/" + smokepingSource + "/",
-            success: function(data) {
-                populateDropdown("#drpDest", data, smokepingDest);
+        /* VERY IMPORTANT: don't use object:contains() here! It doesn't do an
+         * exact match, so if you have two text options with similar names you can
+         * often end up with the wrong option being selected.
+         *
+         * The below approach is much more explicit and will perform an exact
+         * match only.
+         */
+        $(ddName + " option").each(function() {
+            if ($(this).text() == selected) {
+                $(this).attr('selected', 'selected');
+                return;
+            } else {
+                $(this).attr('selected', false);
             }
         });
     }
 
-    /* Reset second dropdown */
-    if (object.name == "source" && object.value == "--SELECT--") {
-        $('#drpDest').empty();
-        $('<option value="--SELECT--">--SELECT--</option>').appendTo("#drpDest");
-        $('#drpDest').attr('disabled', '');
+    this.populateDropdown = function(name, data, selected) {
+        /* Clear the current population */
+        $(name).empty();
+        $(name).append(
+                "<option value=\"--SELECT--\">--SELECT--</option>");
+        $.each(data, function(index, dst){
+                $("<option value=\"" + dst + "\">" + dst +
+                    "</option>").appendTo(name);
+                });
+
+        /* Enable the dropdown */
+        $(name).removeAttr('disabled');
+        this.sortDropdown(name, selected);
+
     }
 
-
-    /* Get the stream ID from the selection and return it */
-    if (smokepingSource != "" && smokepingDest != "") {
-        $.ajax({
-            url: "/api/_streams/rrd-smokeping/" + smokepingSource + "/" + smokepingDest + "/",
-            success: function(data) {
-                changeGraph({graph:"rrd-smokeping", stream:data});
-                updatePageURL(true);
-            }
-       });
-    }
 
 }
 
-// vim: set smartindent shiftwidth=4 tabstop=4 softtabstop=4 expandtab :
+/* "abstract functions" that need to be implemented by child classes */
+Dropdown.prototype.getSelected = function() {};
+Dropdown.prototype.getDropdownState = function() { return {};};
+Dropdown.prototype.setDropdownState = function(state) {};
+Dropdown.prototype.callback = function(object) {};
 
+// vim: set smartindent shiftwidth=4 tabstop=4 softtabstop=4 expandtab :
---
+++ ampweb/static/scripts/dropdown_munin.js
@@ -0,0 +1,113 @@
+function MuninDropdown() {
+    Dropdown.call(this);
+    
+    this.munin = {"switch":"", "interface":"", "direction":""};
+    this.getSelected();
+    this.sortDropdown("#drpSwitch", this.munin["switch"]);
+    this.sortDropdown("#drpInterface", this.munin["interface"]);
+    this.sortDropdown("#drpDirection", this.munin["direction"]);
+}
+
+MuninDropdown.prototype = new Dropdown();
+MuninDropdown.prototype.constructor = MuninDropdown;
+
+MuninDropdown.prototype.getSelected = function() {
+    if ($("#drpSwitch option:selected").text() != "--SELECT--") {
+        this.munin["switch"] = $("#drpSwitch option:selected").text();
+    } else {
+        this.munin["switch"] = "";
+    }
+    if ($("#drpInterface option:selected").text() != "--SELECT--") {
+        this.munin["interface"] = $("#drpInterface option:selected").text();
+    } else {
+        this.munin["interface"] = "";
+    }
+
+    if ($("#drpDirection option:selected").text() != "--SELECT--") {
+        this.munin["direction"] = $("#drpDirection option:selected").text();
+    } else {
+        this.munin["direction"] = "";
+    }
+}
+
+MuninDropdown.prototype.getDropdownState = function() {
+    var obj = {
+        "type" : "munin",
+        "switch" : this.munin["switch"],
+        "interface":  this.munin["interface"],
+        "direction" : this.munin["direction"]
+    };
+
+    return obj;
+}
+
+MuninDropdown.prototype.setDropdownState = function(state) {
+    this.munin["switch"] = state["switch"];
+    this.munin["interface"] = state["interface"];
+    this.munin["direction"] = state["direction"];
+    this.sortDropdown("#drpSwitch", this.munin["switch"]);
+    this.sortDropdown("#drpInterface", this.munin["interface"]);
+    this.sortDropdown("#drpDirection", this.munin["direction"]);
+
+}
+
+MuninDropdown.prototype.callback = function(object) {
+    this.getSelected();
+    var ddobj = this;
+
+    if (object.name == "switch") {
+        $("#drpInterface").empty();
+        $('<option value="--SELECT--">--SELECT--</option>').appendTo("#drpInterface");
+        $("#drpInterface").attr('disabled', '');
+        $("#drpDirection").empty();
+        $('<option value="--SELECT--">--SELECT--</option>').appendTo("#drpDirection");
+        $("#drpDirection").attr('disabled', '');
+
+        this.munin["interface"] = "";
+        this.munin["direction"] = "";
+
+        if (this.munin["switch"] != "") {
+            /* Populate the interfaces dropdown */
+            $.ajax({
+                url: "/api/_destinations/rrd-muninbytes/" + ddobj.munin["switch"] + "/",
+                success: function(data) {
+                    ddobj.populateDropdown("#drpInterface", data, ddobj.munin["interface"]);
+                }
+            });
+        }
+    }
+
+    if (object.name == "interface") {
+        $("#drpDirection").empty();
+        $('<option value="--SELECT--">--SELECT--</option>').appendTo("#drpDirection");
+        $("#drpDirection").attr('disabled', '');
+
+        this.munin["direction"] = "";
+        if (this.munin["interface"] != "") {
+            /* Populate the directions dropdown */
+
+            $.ajax({
+                url: "/api/_destinations/rrd-muninbytes/" + ddobj.munin["switch"] + "/" + this.munin["interface"] + "/",
+                success: function(data) {
+                    ddobj.populateDropdown("#drpDirection", data, ddobj.munin["direction"]);
+                }
+            });
+        }
+    }
+
+    if (this.munin["direction"] != "") {
+        $.ajax({
+            url: "/api/_streams/rrd-muninbytes/" + this.munin["switch"] + "/" + this.munin["interface"] + "/" + this.munin["direction"] + "/",
+            success: function(data) {
+                changeGraph({graph:"rrd-muninbytes", stream:data});
+                updatePageURL(true);
+
+            }
+       });
+
+    }
+
+
+}
+
+// vim: set smartindent shiftwidth=4 tabstop=4 softtabstop=4 expandtab :
