83a606fd3ab7e770bcce1acfd6cc017cd81766ed
Brendon Jones <brendonj@waikato.ac.nz>
2013-09-11 18:23:35 +1200

    Fetch all cell data for a particular duration in a single query.

--- ampweb/views/apifunctions/matrixapi.py
+++ ampweb/views/apifunctions/matrixapi.py
@@ -51,55 +51,47 @@ def matrix(NNTSCConn, request):
             {"_requesting": "sources", "mesh": src_mesh})
 
     tableData = []
-    # Query for data between every source and destination
+
+    stream_ids = []
     for src in sources:
-        stream_ids = []
-        rowData = [src]
         # Get all the destinations that are in this mesh. We can't exclude
         # the site we are testing from because otherwise the table won't
         # line up properly - it expects every cell to have data
         destinations = NNTSCConn.get_selection_options(collection,
                 {"_requesting": "destinations", "mesh": dst_mesh})
-
         # build a list of all streams from this source
         for dst in destinations:
             stream_ids.append(_get_stream_id(streams, src, dst, subtest))
 
-        # query for all the recent information from these streams in one go
-        recent_data = NNTSCConn.get_recent_data(
-                collection, stream_ids, duration, "matrix")
-# XXX do something if recent data is empty?
-        # if it's the latency test then we also need the last 24 hours of data
-        if test == "latency":
-            day_data = NNTSCConn.get_recent_data(
-                    collection, stream_ids, 86400, "matrix")
-        else:
-            day_data = [[]] * len(stream_ids)
+    # query for all the recent information from these streams in one go
+    recent_data = NNTSCConn.get_recent_data(
+            collection, stream_ids, duration, "matrix")
 
-        # combine stream ids, recent data and daily data for each cell
-        for recent,stream_id,day in zip(recent_data,stream_ids,day_data):
-            # stream_id is always the first item, regardless of test. If the
-            # src/dst pair don't test to each other then this will be -1
+    # XXX do something if recent data is empty?
+
+    # if it's the latency test then we also need the last 24 hours of data
+    # so that we can colour the cell based on how it compares
+    if test == "latency":
+        day_data = NNTSCConn.get_recent_data(
+                collection, stream_ids, 86400, "matrix")
+
+    # put together all the row data for DataTables
+    for src in sources:
+        rowData = [src]
+        for dst in destinations:
+            # TODO is this slow, should we store a list to reuse here?
+            stream_id = _get_stream_id(streams, src, dst, subtest)
             value = [stream_id]
-            if stream_id < 0 or len(recent) < 1:
+            if stream_id < 0 or len(recent_data[stream_id]) < 1:
                 value.append(-1)
             else:
-                recent = recent[0]# XXX multiple results because of modulo math
-                assert(recent["stream_id"] == stream_id)
+                recent = recent_data[stream_id][0]
                 if test == "latency":
                     # items are: recent_rtt, daily_rtt, daily_stddev
+                    day = day_data[stream_id][0]
                     value.append(int(round(recent["rtt_avg"] or -1)))
-                    value.append(int(round(day[0]["rtt_avg"] or -1)))
-                    value.append(round(day[0]["rtt_stddev"] or 0))
-
-
-                    #recent = int(round(data["rtt_avg"] or -1))
-                    #foo = day[0] # XXX modulo
-                    #daily_avg = int(round(foo["rtt_avg"] or -1))
-                    #daily_stddev = round(foo["rtt_stddev"] or 0)
-                    #value.append(recent)
-                    #value.append(daily_avg)
-                    #value.append(daily_stddev)
+                    value.append(int(round(day["rtt_avg"] or -1)))
+                    value.append(round(day["rtt_stddev"] or 0))
                 elif test == "loss":
                     value.append(int(round(recent["loss_avg"] * 100)))
                 elif test == "hops":
@@ -108,13 +100,13 @@ def matrix(NNTSCConn, request):
                     else:
                         value.append(-1)
             rowData.append(value)
-
         tableData.append(rowData)
 
     # Create a dictionary to store the data in a way that DataTables expects
     data_list_dict = {}
     data_list_dict.update({'aaData': tableData})
     return data_list_dict
+
 
 def matrix_axis(NNTSCConn, request):
     """ Internal matrix thead specific API """
