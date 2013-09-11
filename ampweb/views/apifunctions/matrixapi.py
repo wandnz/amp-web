4a7574ee886e6f25e8d266f35407a06649382bb2
Brendon Jones <brendonj@waikato.ac.nz>
2013-09-11 16:50:41 +1200

    First pass at cutting down the number of nntsc connections.
    Functions for fetching data now take lists of stream_ids and will fetch all the data at once. This is currently used to fetch data per row, soon to be combined into a single fetch.

--- ampweb/views/apifunctions/matrixapi.py
+++ ampweb/views/apifunctions/matrixapi.py
@@ -53,65 +53,62 @@ def matrix(NNTSCConn, request):
     tableData = []
     # Query for data between every source and destination
     for src in sources:
+        stream_ids = []
         rowData = [src]
         # Get all the destinations that are in this mesh. We can't exclude
         # the site we are testing from because otherwise the table won't
         # line up properly - it expects every cell to have data
         destinations = NNTSCConn.get_selection_options(collection,
                 {"_requesting": "destinations", "mesh": dst_mesh})
+
+        # build a list of all streams from this source
         for dst in destinations:
-            # Get IPv4 data
-            stream_id = _get_stream_id(streams, src, dst, subtest)
-            if stream_id > 0:
-                result4 = NNTSCConn.get_recent_data(collection, stream_id, duration, "matrix")
-                if result4.count() > 0:
-                    queryData = result4.fetchone()
-                    value = [stream_id]
-                    if test == "latency":
-                        recent = int(round(queryData["rtt_avg"] or -1))
-                        # Get the last weeks average for the dynamic scale
-                        result_24_hours = NNTSCConn.get_recent_data(
-                                collection, stream_id, 86400, "matrix")
-                        day_data = result_24_hours.fetchone()
-                        daily_avg = int(round(day_data["rtt_avg"] or -1))
-                        daily_stddev = round(day_data["rtt_stddev"] or 0)
-                        value.append(recent)
-                        value.append(daily_avg)
-                        value.append(daily_stddev)
-                    elif test == "loss":
-                        value.append(int(round(queryData["loss_avg"] * 100)))
-                    elif test == "hops":
-                        if queryData["length"]:
-                            value.append(int(queryData["length"]))
-                        else:
-                            value.append(-1)
-                    rowData.append(value)
-                else:
-                    # This marks src/dst combinations that do test to each
-                    # other (they have a stream_id) but there is no recent
-                    # data for some reason
-                    rowData.append([stream_id, -1])
+            stream_ids.append(_get_stream_id(streams, src, dst, subtest))
+
+        # query for all the recent information from these streams in one go
+        recent_data = NNTSCConn.get_recent_data(
+                collection, stream_ids, duration, "matrix")
+# XXX do something if recent data is empty?
+        # if it's the latency test then we also need the last 24 hours of data
+        if test == "latency":
+            day_data = NNTSCConn.get_recent_data(
+                    collection, stream_ids, 86400, "matrix")
+        else:
+            day_data = [[]] * len(stream_ids)
+
+        # combine stream ids, recent data and daily data for each cell
+        for recent,stream_id,day in zip(recent_data,stream_ids,day_data):
+            # stream_id is always the first item, regardless of test. If the
+            # src/dst pair don't test to each other then this will be -1
+            value = [stream_id]
+            if stream_id < 0 or len(recent) < 1:
+                value.append(-1)
             else:
-                # This value marks src/dst combinations that do not have data
-                # because they do not test to each other
-                rowData.append([-1, -1])
-            # Get IPv6 data
-            # src6 = src + ":v6"
-            # dst6 = dst + ":v6"
-            # result6 = conn.get_recent_data(src6, dst6, ampy_test, subtest,
-            #        duration)
-            # if result6.count() > 0:
-                # queryData = result6.fetchone()
-                # if test == "latency":
-                    # value = int(round(queryData[index][sub_index]))
-                # elif test == "loss":
-                    # missing = queryData[index]["missing"]
-                    # present = queryData[index]["count"]
-                    # loss = 100.0 * missing / (missing + present)
-                    # value = int(round(loss))
-                # rowData.append(value)
-            # else:
-                # rowData.append("X")
+                recent = recent[0]# XXX multiple results because of modulo math
+                assert(recent["stream_id"] == stream_id)
+                if test == "latency":
+                    # items are: recent_rtt, daily_rtt, daily_stddev
+                    value.append(int(round(recent["rtt_avg"] or -1)))
+                    value.append(int(round(day[0]["rtt_avg"] or -1)))
+                    value.append(round(day[0]["rtt_stddev"] or 0))
+
+
+                    #recent = int(round(data["rtt_avg"] or -1))
+                    #foo = day[0] # XXX modulo
+                    #daily_avg = int(round(foo["rtt_avg"] or -1))
+                    #daily_stddev = round(foo["rtt_stddev"] or 0)
+                    #value.append(recent)
+                    #value.append(daily_avg)
+                    #value.append(daily_stddev)
+                elif test == "loss":
+                    value.append(int(round(recent["loss_avg"] * 100)))
+                elif test == "hops":
+                    if recent["length"]:
+                        value.append(int(recent["length"]))
+                    else:
+                        value.append(-1)
+            rowData.append(value)
+
         tableData.append(rowData)
 
     # Create a dictionary to store the data in a way that DataTables expects
