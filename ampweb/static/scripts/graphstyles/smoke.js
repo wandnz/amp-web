75a12671f9be3f8d76eaf75a847d6077c4581e88
Brendon Jones <brendonj@waikato.ac.nz>
2013-10-07 16:17:39 +1300

    Update smokeping findMaximumY() to deal with multiple data series.

--- ampweb/static/scripts/graphstyles/smoke.js
+++ ampweb/static/scripts/graphstyles/smoke.js
@@ -13,39 +13,65 @@ function SmokepingGraph(params) {
      * just the median */
     this.findMaximumY = function(data, start, end) {
         var maxy = 0;
-        var startind, i, j;
+        var startind, i, j, series;
 
-        startind = null;
-        for (i = 0; i < data.length; i++) {
-            if (startind === null) {
-                if (data[i][0] >= start * 1000) {
-                    startind = i;
+        for ( series = 0; series < data.length; series++ ) {
+            startind = null;
+            if ( data[series].length == 0 ) {
+                continue;
+            }
+            for ( i = 0; i < data[series].data.length; i++ ) {
+                if ( startind === null ) {
+                    /* ignore values until we find one in range of the graph */
+                    if ( data[series].data[i][0] >= start * 1000 ) {
+                        startind = i;
 
-                    if (i != 0) {
-                        for (j = 3; j < data[i].length; j++) {
-                            if (data[i - 1][j] == null)
-                                continue
-                            if (data[i - 1][j] > maxy)
-                                maxy = data[i - 1][j];
+                        /*
+                         * Check out the value immediately preceding the graph
+                         * as we will be drawing a line to it - if it's big
+                         * then we probably want to update max y.
+                         */
+                        if ( i != 0 ) {
+                            for (j = 3; j < data[series].data[i].length; j++) {
+                                if ( data[series].data[i - 1][j] == null ) {
+                                    continue;
+                                }
+                                if ( data[series].data[i - 1][j] > maxy ) {
+                                    maxy = data[series].data[i - 1][j];
+                                }
+                            }
                         }
+                    } else {
+                        /* data still out of graph range, continue */
+                        continue;
                     }
-                } else {
-                    continue;
                 }
-            }
 
-            for (j = 3; j < data[i].length; j++) {
-                if (data[i][j] == null)
-                    continue
-                if (data[i][j] > maxy)
-                    maxy = data[i][j];
-            }
+                /* our data is now fully within the graph, check it all */
+                for ( j = 3; j < data[series].data[i].length; j++ ) {
+                    if ( data[series].data[i][j] == null ) {
+                        continue;
+                    }
+                    if ( data[series].data[i][j] > maxy ) {
+                        maxy = data[series].data[i][j];
+                    }
+                }
 
-            if (data[i][0] > end * 1000)
-                break;
+                /*
+                 * Stop *after* we have found a value outside the range of the
+                 * graph so that we check the values just off the right hand
+                 * side in the same way we did the left.
+                 */
+                if ( data[series].data[i][0] > end * 1000 ) {
+                    break;
+                }
+
+            }
         }
-        if (maxy == 0 || maxy == null)
+
+        if ( maxy == 0 || maxy == null ) {
             return 1;
+        }
 
         return maxy;
     }
