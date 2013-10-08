e3d1dc6cbb07bff2e7a46706f3a5fcf92a3b4142
Brendon Jones <brendonj@waikato.ac.nz>
2013-10-09 12:01:09 +1300

    Added TODO notes about how events could be improved.

--- ampweb/static/scripts/events.js
+++ ampweb/static/scripts/events.js
@@ -16,6 +16,10 @@
  * this seems an acceptable way to reduce the clutter.
  *
  * TODO make this draw behind the main time series data rather than on top
+ * TODO can we do something smart here that means it is only drawn once rather
+ * than for every single series that has it enabled? Would be nice to enable
+ * it globally and not have to disable it on every series except an empty
+ * dummy series.
  */
 Flotr.addType('events', {
     options: {
