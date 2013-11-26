e90d81d55a7a4044dc56be128ae8053434b567ec
Brendon Jones <brendonj@waikato.ac.nz>
2013-11-26 19:13:50 +1300

    Remove old format_data functions that aren't required.

--- ampweb/views/collections/amptraceroute.py
+++ ampweb/views/collections/amptraceroute.py
@@ -32,67 +32,6 @@ class AmpTracerouteGraph(AmpIcmpGraph):
                 results[line].append(result)
         return results
 
-    def format_data_2(self, data):
-        results = {}
-
-        # XXX This will need to also create a list of hops at some point
-        # for drawing the rainbow graph. I'll leave that up to whoever is
-        # implementing it to figure out exactly what format they need it in
-        for line, datapoints in data.iteritems():
-            results[line] = []
-            for datapoint in datapoints:
-                result = [datapoint["timestamp"] * 1000]
-                if "length_avg" in datapoint:
-                    result.append(round(datapoint["length_avg"]))
-                else:
-                    result.append(None)
-                results[line].append(result)
-        return results
-
-    def format_data_old(self, data):
-        results = {}
-
-        # XXX This will need to also create a list of hops at some point
-        # for drawing the rainbow graph. I'll leave that up to whoever is
-        # implementing it to figure out exactly what format they need it
-        # in
-        for address,stream_data in data.iteritems():
-            # store all the measurements within each bin for averaging
-            length = {} # XXX new
-            # Store the last timestamp with data in each bin for plotting. We
-            # need to use binstart to group the data as they are at fixed,
-            # known times, but need to plot using the last timestamp so that
-            # data actually appears to be recent.
-            last = {}
-            for line, datapoints in stream_data.iteritems():
-                for datapoint in datapoints:
-                    ts = datapoint["binstart"] * 1000
-                    if ts not in length:
-                        length[ts] = []
-                        last[ts] = datapoint["timestamp"] * 1000
-
-                    if "length_avg" in datapoint:
-                        #length[ts].append(float(datapoint["length_avg"]))
-                        length[ts] += [float(datapoint["length_avg"])] * datapoint["length_count"]
-                    else:
-                        length[ts].append(None)
-                    if datapoint["timestamp"] * 1000 > last[ts]:
-                        last[ts] = datapoint["timestamp"] * 1000
-
-            results[address] = []
-            timestamps = length.keys()
-            timestamps.sort()
-            for ts in timestamps:
-                pathlen = length[ts]
-                # Only calculate length stats for valid values (i.e not None)
-                valid = [x for x in pathlen if x is not None]
-                if len(valid) > 0:
-                    avg_path = sum(valid) / len(valid)
-                else:
-                    avg_path = None
-                results[address].append([last[ts], avg_path])
-        return results
-
     def get_dropdowns(self, NNTSCConn, streamid, streaminfo):
 
         return super(AmpTracerouteGraph, self).get_dropdowns(NNTSCConn,
@@ -107,7 +46,7 @@ class AmpTracerouteGraph(AmpIcmpGraph):
     def get_event_label(self, event):
         # TODO Include the address in the event text
         target = event["target_name"].split("|")
-        
+
         label = "AMP Traceroute: " + event["event_time"].strftime("%H:%M:%S")
         label += " %s " % event["type_name"]
         label += "from %s to " % (event["source_name"])
