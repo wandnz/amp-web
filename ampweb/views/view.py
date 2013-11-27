a3ec77c07ffe288ff6fceffe0d3cd9ef500ba922
Brendon Jones <brendonj@waikato.ac.nz>
2013-11-27 14:11:04 +1300

    Add rainbow graph javascript to the views interface.

--- ampweb/views/view.py
+++ ampweb/views/view.py
@@ -17,6 +17,7 @@ stylescripts = [
     "graphstyles/config.js",
     "graphstyles/basicts.js",
     "graphstyles/smoke.js",
+    "graphstyles/rainbow.js",
 ]
 
 pagescripts = [
@@ -96,6 +97,7 @@ def generateGraph(graph, url):
         "events.js",
         "selection.js",
         "smokeping.js",
+        "rainbow.js",
     ]
 
     scripts += stylescripts
