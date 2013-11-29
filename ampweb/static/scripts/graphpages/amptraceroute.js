ef1ca40363a2c3cc3ab99b4896a03dbe86cf8c3d
Brendon Jones <brendonj@waikato.ac.nz>
2013-11-29 13:56:23 +1300

    Make the default traceroute graph use the smokeping style.

--- ampweb/static/scripts/graphpages/amptraceroute.js
+++ ampweb/static/scripts/graphpages/amptraceroute.js
@@ -13,7 +13,8 @@ AmpTracerouteGraphPage.prototype.initDropdowns = function(stream) {
 }
 
 AmpTracerouteGraphPage.prototype.drawGraph = function(start, end, first) {
-
+    /* TODO create another traceroute page that will show the rainbow graph */
+    /*
     this.graph = new RainbowGraph({
         container: $("#graph"),
         start: start,
@@ -27,6 +28,20 @@ AmpTracerouteGraphPage.prototype.drawGraph = function(start, end, first) {
         measureLatency: false,
         minHopHeight: 5
     });
+    */
+
+    this.graph = new SmokepingGraph({
+        container: $("#graph"),
+        start: start,
+        end: end,
+        firstts: first,
+        lines: [ {id:this.view} ], //XXX to work with existing streams code
+        urlbase: API_URL + "/_view/amp-traceroute/",
+        event_urlbase: API_URL + "/_event/amp-traceroute/",
+        miny: 0,
+        ylabel: "Number of Hops",
+    });
+
 
     this.graph.createGraphs();
 }
