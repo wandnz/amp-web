72e0f4862a58d75f3d688bf7e68e3826c63f8c9c
Shane Alcock <salcock@waikato.ac.nz>
2014-01-21 18:08:02 +1300

    Don't try to do everything in processSummaryData
    This function is extended by RainbowGraph and the order of
    operations gets broken if we start trying to draw things before
    the RainbowGraph finishes converting our data into the format
    it requires.

--- ampweb/static/scripts/graphstyles/basicts.js
+++ ampweb/static/scripts/graphstyles/basicts.js
@@ -232,7 +232,31 @@ function BasicTimeSeriesGraph(params) {
         }
     }
 
+    this.receivedSummaryData = function (sumdata) {
+        var sumopts = this.summarygraph.options;
 
+        this.processSummaryData(sumdata);
+        if (!this.summarygraph.dataAvail) {
+            this.processSummaryEvents();
+            this.determineSummaryStart();
+            this.setSummaryAxes();
+        }
+        
+        if ( this.maxy == null ) {
+            sumopts.config.yaxis.max = this.findMaximumY(sumopts.data,
+                    this.summarygraph.start, this.summarygraph.end) * 1.1;
+        }
+
+        if (this.summarycomponent == null)
+            createEnvision(this);
+        this.drawSummaryGraph();
+
+        if (this.detailgraph.dataAvail) {
+            this.mergeDetailSummary();
+            this.drawDetailGraph();
+        }
+        this.summarygraph.dataAvail = true;
+    }
 
     /* Queries for data required to draw the summary graph. */
     this.fetchSummaryData = function() {
@@ -266,13 +290,13 @@ function BasicTimeSeriesGraph(params) {
 
         if (fetchstart > this.summarygraph.start) {
             this.summaryreq = $.getJSON(url, function(sumdata) {
-                graph.processSummaryData(sumdata);
+                graph.receivedSummaryData(sumdata);
             }).then(function() {
                 return graph.fetchSummaryData();
             });
         } else {
             this.summaryreq = $.getJSON(url, function(sumdata) {
-                graph.processSummaryData(sumdata);
+                graph.receivedSummaryData(sumdata);
             });
         }
 
@@ -601,28 +625,6 @@ function BasicTimeSeriesGraph(params) {
             newdata = sumdata[name].concat(series.data);
             series.data = newdata;
         });
-
-        if (!this.summarygraph.dataAvail) {
-            this.processSummaryEvents();
-            this.determineSummaryStart();
-            this.setSummaryAxes();
-        }
-
-        if ( this.maxy == null ) {
-            sumopts.config.yaxis.max = this.findMaximumY(sumopts.data,
-                    this.summarygraph.start, this.summarygraph.end) * 1.1;
-        }
-
-        if (this.summarycomponent == null)
-            createEnvision(this);
-        this.drawSummaryGraph();
-
-        if (this.detailgraph.dataAvail) {
-            this.mergeDetailSummary();
-            this.drawDetailGraph();
-        }
-        this.summarygraph.dataAvail = true;
-
 
     }
 
