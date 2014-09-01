1f9155a9289462d23867bdd049b2115410b7cce4
Brendon Jones <brendonj@waikato.ac.nz>
2014-09-01 18:15:37 +1200

    Include test arguments when the form is submitted.

--- ampweb/views/apifunctions/scheduleapi.py
+++ ampweb/views/apifunctions/scheduleapi.py
@@ -1,3 +1,37 @@
+import base64
+import re
+import getopt
+
+def validate_args(test, args):
+    testopts = {
+        "icmp": "s:",
+        "traceroute": "I:abfp:rs:S:4:6:",
+        "dns": "I:q:t:c:z:rsn4:6:",
+        "tcpping": "I:p:P:rs:S:4:6:",
+    }
+
+    if test not in testopts:
+        return None
+
+    optstring = testopts[test]
+
+    # We don't care about the actual arguments and what their values are, we
+    # only care that it was able to be parsed 100%. The test itself will
+    # double check that it has sane values for each option. Maybe we should
+    # do more here, so we don't end up with tests that will fail on startup.
+    try:
+        parsed, remaining = getopt.getopt(args.split(), optstring)
+        # make sure all arguments were parsed
+        if len(remaining) > 0:
+            return None
+    except getopt.GetoptError:
+        # any error parsing causes this to fail
+        return None
+
+    # the original arg string is fine, return it
+    return args
+
+
 def schedule_test(ampy, request):
     urlparts = request.matchdict['params']
     print urlparts
@@ -17,7 +51,9 @@ def schedule_test(ampy, request):
         start = urlparts[6]
         end = urlparts[7]
         period = urlparts[8]
-        args = urlparts[9]
+        args = validate_args(test, base64.b64decode(urlparts[9]))
+        if args is None:
+            return
         # return the id of the new view, creating it if required
         return ampy.schedule_new_amp_test(src, dst, test, freq, start, end,
                 period, args)
