3e723142429279afa2d3fabfd233df828600f64d
Brendon Jones <brendonj@waikato.ac.nz>
2015-12-03 14:58:44 +1300

    Serve cacert from /static/, easier to make permissions work.

--- ampweb/__init__.py
+++ ampweb/__init__.py
@@ -51,9 +51,7 @@ def main(global_config, **settings):
     # Static content
     config.add_static_view('static', 'ampweb:static/', cache_max_age=3600)
     config.add_static_view('fonts', 'ampweb:static/fonts/', cache_max_age=3600)
-    # XXX it gets upset if a file doesn't exist, may need to do something
-    # to make the static certificate work in the general case
-    config.add_asset_views('ampweb:static', filenames=['robots.txt', 'cacert.pem'], http_cache=3600)
+    config.add_asset_views('ampweb:static', filenames=['robots.txt'], http_cache=3600)
 
     # Dynamic content from views
     config.add_route('home', '/')
