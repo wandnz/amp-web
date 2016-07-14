6cdb821a7feac4dcaf09d55ebd93ed814c42a157
Shane Alcock <salcock@waikato.ac.nz>
2016-07-15 10:07:08 +1200

    Prevent multiple panels for the same group appearing on the dashboard.
    It appears that the bug in the eventparser that means a group gets added
    to the group list multiple times may not be entirely fixed. Until I can
    create an easily replicable test case to track it down properly, this
    code will at least try to prevent the group from appearing multiple
    times on the dashboard.

--- ampweb/static/scripts/eventgroups/events.js
+++ ampweb/static/scripts/eventgroups/events.js
@@ -1059,7 +1059,10 @@ function fetchDashEvents(clear, endtime) {
                 else
                     knowngroups[gid].panelopen = false;
 
-                if ($(panelid).length)
+                /* Remove all panels with this group ID, just in case
+                 * we still manage to end up with more than one.
+                 */
+                while ($(panelid).length)
                     $(panelid).remove();
 
             }
@@ -1071,6 +1074,16 @@ function fetchDashEvents(clear, endtime) {
             var panelopen = false;
 
             if (group.ts < eventfiltering.starttime)
+                continue;
+
+            /* Just in case we fetch multiple groups with the same ID,
+             * avoid adding a panel for a group more than once.
+             *
+             * TODO figure out why this sometimes happens, must be some
+             * weird case in eventparser.py that prevents a group from
+             * being purged when it should be.
+             */
+            if ($(panelid).length)
                 continue;
 
             if (dashmin == 0) {
