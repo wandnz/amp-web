44f06d0f296fae208cead9b920c62d30498d4b1e
Shane Alcock <salcock@waikato.ac.nz>
2017-02-13 15:47:37 +1300

    Add "family" support to RRD Smokeping collection
    Also added ability to download "raw" smokeping data.

--- ampweb/static/scripts/modals/smokeping_modal.js
+++ ampweb/static/scripts/modals/smokeping_modal.js
@@ -4,28 +4,31 @@ function SmokepingModal(/*stream*/) {
 
 SmokepingModal.prototype = new Modal();
 SmokepingModal.prototype.constructor = SmokepingModal;
-
 SmokepingModal.prototype.collection = "rrd-smokeping";
+
 SmokepingModal.prototype.selectables = [
     {name:"source", label:"source", type:"dropdown"},
-    {name:"host", label:"target", type:"dropdown"}
+    {name:"host", label:"target", type:"dropdown"},
+    {name:"aggregation", label:"address family", type:"fixedradio"}
 ];
 
 SmokepingModal.prototype.update = function(name) {
     switch ( name ) {
         case "source": this.updateModalDialog(name); break;
-        case "host": this.updateSubmit(); break;
+        case "host": this.updateModalDialog(name); break;
+        case "aggregation": this.updateFixedRadio(); break
         default: this.updateModalDialog(name); break;
     };
 }
 
 SmokepingModal.prototype.submit = function() {
     /* get new view id */
-    var source, destination;
+    var source, destination, family;
     var source = this.getDropdownValue("source");
     var destination = this.getDropdownValue("host");
+    var family = this.getRadioValue('aggregation');
 
-    this.submitAjax([source, destination], this.collection);
+    this.submitAjax([source, destination, family], this.collection);
 }
 
 // vim: set smartindent shiftwidth=4 tabstop=4 softtabstop=4 expandtab :
