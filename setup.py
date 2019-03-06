b4a75670ee50fed114391e1fc5ef62c947c78fde
Brendon Jones <brendonj@waikato.ac.nz>
2019-03-07 11:24:05 +1300

    Bump version number to 1.17 and update changelog.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ requires = [
     ]
 
 setup(name='amp-web',
-      version='1.15',
+      version='1.17',
       description='Web interface for exploring data stored in nntsc',
       classifiers=[
         "Programming Language :: Python",
